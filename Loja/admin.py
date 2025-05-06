ce378c433fee530d9a0d9d68dff9d5e3363636f7
RaicaDandara <granjeiro.raica@escolar.ifrn.edu.br>
2025-05-06 15:28:04 -0300

    Fim do Capítulo 4 (verificar a parte do filtro por datas)

--- Loja/admin.py
+++ Loja/admin.py
@@ -4,8 +4,22 @@ from Loja.models import Fabricante
 from django.contrib import admin #isso já vai estar existindo noarquivo
 # Register your models here.
 from .models import * #imporata nossos models
+class FabricanteAdmin(admin.ModelAdmin):
+    # Cria um filtro de hierarquia com datas
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    msgPromocao = models.CharField(null=True, max_length=100, blank=True)
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao',
+    'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    # fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    # exclude = ('msgPromocao')
+    # search_fields = ('Produto',):
+
 
 # Register your models here.
-admin.site.register(Fabricante)
+admin.site.register(Fabricante,FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
