ed5115cf4afed0e839a6b3d0b1fad60fdb367218
Raica Dandara <granjeiro.raica@escolar.ifrn.edu.br>
2025-05-12 15:18:02 -0300

    Tutoriais e Observações (um lembrete para o futuro)

---
+++ Loja/__init__.py
@@ -0,0 +1,22 @@
+# OBS: para reinstalar o que precisa sempre que clonar esse repositório ou mexer nele com pull:
+# pip install django Django==4.2.7
+# pip install Pillow
+
+# ENVIAR É
+# 1. git add .
+# 2. git status (apenas verificando)
+# 3. git commit -m "mensagem de sua preferência"
+# 4. git push
+
+# ATUALIZAR É
+# 1. git status (apenas para verificar)
+# 2. git pull
+
+# NOVA MÁQUINA É
+# 1. git config --global user.name “nome do usuário"
+# 2. git config --global user.email seuemail@email.com
+# 3. git clone https://github.com/RaicaDandara/IFRN_Programacao-Web.git
+# 4. Refazer o ambiente virtual
+#       4.1 python -m venv venv
+#       4.2 pip install django Django==4.2.7
+#       4.3 pip install Pillow
--- Loja/views/ProdutoView.py
+++ Loja/views/ProdutoView.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 def list_produto_view(request, id=None):
     if id is None:
         return HttpResponse('<h1>Nenhum id foi informado</h1>')
+    # ou se preferir
+    # if id is None:
+    #     id = 0
     return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    # OBS: o % indica que uma variável (nesse cado id, pq vem após o |% vai ser inserida no meio da string formatada criada)
